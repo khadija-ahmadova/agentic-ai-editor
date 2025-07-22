a0eee97fe5476aff59ba37ecd1289144e00a2893
khadija-ahmadova <xadija.ahmedova@gmail.com>
2025-07-22 19:14:59 +0400

    fix: absolute path

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -23,22 +23,26 @@ def get_files_info(working_directory, directory="."):
     except Exception as e:
         return f"Error: {e}"
 
-    
+
 def get_file_content(working_directory, file_path):
     try:
+        # print(working_directory)
+        # print(file_path)
         working_directory = os.path.abspath(working_directory)
-        file_path = os.path.abspath(file_path)
+        full_path = os.path.abspath(os.path.join(working_directory, file_path))
+        # print(working_directory)
+        # print(file_path)
 
-        if not file_path.startswith(working_directory):
+        if not full_path.startswith(working_directory):
             return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
         
-        if not os.path.isfile(file_path):
+        if not os.path.isfile(full_path):
             return f'Error: File not found or is not a regular file: "{file_path}"'
         
-        with open(file_path, "r", encoding="utf-8") as file:
+        with open(full_path, "r", encoding="utf-8") as file:
             file_content_string = file.read()
         if len(file_content_string) > MAX_CHARS:
-            content = content[:MAX_CHARS] + f'\n[...File "{file_path}" truncated at 10000 characters]'
+            file_content_string = file_content_string[:MAX_CHARS] + f'\n[...File "{file_path}" truncated at 10000 characters]'
         return file_content_string
 
     except Exception as e:
