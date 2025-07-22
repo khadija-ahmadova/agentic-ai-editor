23f0a448b0d9cd9a9085cf833fd0d6b672ae237e
khadija-ahmadova <xadija.ahmedova@gmail.com>
2025-07-22 18:37:58 +0400

    add function to get file content

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,5 @@
 import os
-
+MAX_CHARS = 10000
 
 def get_files_info(working_directory, directory="."):
     try:
@@ -20,5 +20,26 @@ def get_files_info(working_directory, directory="."):
             content_list.append(f"- {item}: file_size={file_size} bytes, is_dir={is_dir}")
         return "\n".join(content_list)
     
+    except Exception as e:
+        return f"Error: {e}"
+
+    
+def get_file_content(working_directory, file_path):
+    try:
+        working_directory = os.path.abspath(working_directory)
+        file_path = os.path.abspath(file_path)
+
+        if not file_path.startswith(working_directory):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        
+        if not os.path.isfile(file_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        
+        with open(file_path, "r", encoding="utf-8") as file:
+            file_content_string = file.read()
+        if len(file_content_string) > MAX_CHARS:
+            content = content[:MAX_CHARS] + f'\n[...File "{file_path}" truncated at 10000 characters]'
+        return file_content_string
+
     except Exception as e:
         return f"Error: {e}"
